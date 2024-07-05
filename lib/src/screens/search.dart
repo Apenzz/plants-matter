208ed5b8229b54c11366d9177109af1a5b3fa5cf
albertoDev <albypnz@gmail.com>
2024-07-05 17:33:28 +0200

    added list view to search

--- lib/src/screens/search.dart
+++ lib/src/screens/search.dart
@@ -2,23 +2,67 @@ import 'package:flutter/material.dart' hide SearchBar;
 import '../widgets/searchbar.dart';
 
 class SearchScreen extends StatefulWidget {
+
+  const SearchScreen({super.key});
   @override
   _SearchScreenState createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  final List<String> _allItems = [
+    'Apple',
+    'Banana',
+    'Cherry',
+    'Date',
+    'Elderberry',
+    'Fig',
+    'Grapes',
+    'Honeydew',
+    'Kiwi',
+    'Lemon',
+  ];
+  List<String> _filteredItems = [];
+  
+  @override  
+  void initState() {
+    super.initState();
+    _filteredItems = [];
+  }
+
+  void _filterItems(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        _filteredItems = _allItems;
+      } else {
+        _filteredItems = _allItems.where((item) {
+          return item.toLowerCase().contains(query.toLowerCase());
+        }).toList();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Search Plants'),
       ),
-      body: const Column(
+      body: Column(
         children: [
-          SearchBar(),
+          SearchBar(
+            controller: _controller,
+            onChanged: _filterItems,
+          ),
           Expanded(
-            child: Center(
-              child: Text('Search results will be displayed here!'),
+            child: ListView.builder( 
+              itemCount: _filteredItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_filteredItems[index]),
+                );
+              },
             ),
           ),
         ],
