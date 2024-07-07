c3131d0246423d858d608287b68a985a5e3a0cd5
mizzet1 <riccardo.accts@gmail.com>
2024-07-07 18:31:34 +0200

    database-sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
-
 import 'package:flutter/material.dart';
+import 'src/load_json_to_db.dart';
 import 'src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadDatabase();
   runApp(const Plantsmatter());
 }
+
+
+
