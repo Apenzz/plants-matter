d7dcdc3f76d314520b406c9bce038dec745e59df
mizzet1 <riccardo.accts@gmail.com>
2024-07-11 23:28:17 +0200

    listview edit

--- lib/src/load_json_to_db.dart
+++ lib/src/load_json_to_db.dart
@@ -6,13 +6,11 @@ Future<void> loadDatabase() async {
   try {
     final dbHelper = DatabaseHelper.instance;
 
-    // Load the asset manifest to get the list of all asset files
     final manifestContent = await rootBundle.loadString('AssetManifest.json');
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
 
-    // Specify the correct path to your JSON files
     final jsonFiles = manifestMap.keys
-        .where((String key) => key.contains('lib/src/data/json-sample/'))
+        .where((String key) => key.contains('lib/src/data/json/'))
         .toList();
 
     for (String path in jsonFiles) {
