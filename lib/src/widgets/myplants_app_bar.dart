55330bd4797827092d376bc6384f431c08b56f47
mizzet1 <riccardo.accts@gmail.com>
2024-07-11 18:36:43 +0200

    .

--- lib/src/widgets/myplants_app_bar.dart
+++ lib/src/widgets/myplants_app_bar.dart
@@ -1,41 +1,16 @@
 import 'package:flutter/material.dart';
 
 class MyPlantsAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final List<Widget>? actions;
-
-  const MyPlantsAppBar({
-    this.actions,
-    super.key,
-  });
+  const MyPlantsAppBar({super.key});
 
   @override
   Size get preferredSize => AppBar().preferredSize;
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return AppBar(
-      actions: [
-          IconButton(
-            icon: const Icon(Icons.attach_file),
-            onPressed: () {
-              // Implement search button logic here
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.calendar_month),
-            onPressed: () {
-              // Implement add button logic here
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.more_vert),
-            onPressed: () {
-              // Implement settings button logic here
-            },
-          ),
-        ],
+      title: const Text('My Plants'),
+      centerTitle: true,
     );
   }
-
-
 }
